8598a88b86926f47c6ac0d0bc2be65458c9d2213
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-17 09:12:15 +0545

    login backend completed

--- server/controllers/userLogin.js
+++ server/controllers/userLogin.js
@@ -34,7 +34,7 @@ exports.loginUser = async (req, res) => {
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
       );
-      res.status(200).json({ token });
+      res.status(200).json({ token, user });
     });
   } catch (error) {
     res.status(500).json({ message: error.message });

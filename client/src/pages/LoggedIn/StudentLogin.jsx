7a412bd456028d45e028a6d03b79c339a9339bec
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-18 20:30:39 +0545

    dashboard view

--- client/src/pages/LoggedIn/StudentLogin.jsx
+++ client/src/pages/LoggedIn/StudentLogin.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import Navbar from "../../component/Navbar";
 import axios from "axios";
+import Sidebar from "../../component/Sidebar";
 
 const StudentLogin = () => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
     </div>
   );
 };

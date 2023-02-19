0e4c83cf02be6d99891899ce3f18f39410e4f25c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-19 18:00:33 +0545

    admin add student and teacher

--- server/controllers/userLogin.js
+++ server/controllers/userLogin.js
@@ -36,11 +36,66 @@ exports.loginUser = async (req, res) => {
       );
       res.status(200).json({ token: token, user: user });
     });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
 
-exports.registerUser = async (req, res) => {};
+exports.registerUser = async (req, res) => {
+  try {
+    const { role } = req.body;
 
-exports.changePassword = async (req, res) => {};
+    if (role === "teacher") {
+      const { tid, sid, name, email, password } = req.body;
+      let query = `insert into teachers(tid, sid, name, email, password) values('${tid}', '${sid}', '${name}', '${email}', '${password}')`;
+
+      const addTeacher = client.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        res.status(200).json("teacher added successfully");
+      });
+    }
+    if (role === "student") {
+      const { stuid, cid, name, email, password } = req.body;
+      let query = `insert into students(stuid, cid, name, email, password) values('${stuid}', '${cid}', '${name}', '${email}', '${password}')`;
+
+      const addTeacher = client.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        res.status(200).json("student added successfully");
+      });
+    }
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  // change pasword hash and store
+  // =============== REMAINING ===============
+  // try {
+  //   const { email, password } = req.body;
+  //   const query = `select * from users where email = '${email}'`;
+  //   const userExists = client.query(query, (err, result) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     if (result.rows.length === 0) {
+  //       res.status(200).json({ noUser: "User not found" });
+  //     }
+  //     const user = result.rows[0];
+  //     const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+  //     if (!isPasswordCorrect) {
+  //       res.status(200).json({ wrongPassword: "Incorrect password" });
+  //     }
+  //   });
+  // } catch (err) {
+  //   res.status(404).json({ error: err.message });
+  // }
+  res.send("change password");
+};

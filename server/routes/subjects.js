2129041f212d47c6e5c4e38359fa0c0ae5700870
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-19 18:06:21 +0545

    subject according to class route

--- server/routes/subjects.js
+++ server/routes/subjects.js
@@ -2,10 +2,17 @@
 const Router = require("express").Router;
 const router = Router();
 
-const { getSubject, addSubject } = require("../controllers/subjects");
+const {
+  getSubject,
+  addSubject,
+  getSubjectByClass,
+} = require("../controllers/subjects");
 
 // get all subjects
 router.get("/view-subjects", getSubject);
+
+//  get subject by class
+router.get("/view-subjects/:class", getSubjectByClass);
 
 // add subject
 router.post("/add-subject", addSubject);

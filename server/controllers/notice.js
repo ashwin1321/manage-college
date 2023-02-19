d1b67d9e046d8f6f788b1b9c3e6d0fd4bca8c934
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-19 18:19:22 +0545

    controller to add view and delete notices

--- server/controllers/notice.js
+++ server/controllers/notice.js
@@ -1,7 +1,53 @@
 const { connectDb, client } = require("../models/config");
 
-exports.getNotice = async (req, res) => {};
+exports.getNotice = async (req, res) => {
+  try {
+    const query = `select * from notice`;
 
-exports.addNotice = async (req, res) => {};
+    const getnotice = client.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-exports.deleteNotice = async (req, res) => {};
+      res.status(200).json(result.rows);
+    });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+exports.addNotice = async (req, res) => {
+  const { notice } = req.body;
+  try {
+    const query = `insert into notice (notice) values ('${notice}')`;
+
+    const addnotice = client.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.status(200).json({ message: "Notice added successfully" });
+    });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+exports.deleteNotice = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const query = `delete from notice where id = ${id}`;
+
+    const deletenotice = client.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.status(200).json({ message: "Notice deleted successfully" });
+    });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};

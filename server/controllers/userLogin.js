a775c3ae4aed896f9697f9d8c6f6b7cd3c0e50f9
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-20 15:41:05 +0545

    classroom details page created

--- server/controllers/userLogin.js
+++ server/controllers/userLogin.js
@@ -7,35 +7,38 @@ connectDb();
 exports.loginUser = async (req, res) => {
   try {
     const { email, password, role } = req.body;
-    const query = `select * from users where email = '${email}'`;
 
-    const userLogin = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+    if (role === "admin") {
+      const query = `select * from users where email = '${email}'`;
 
-      if (result.rows.length === 0) {
-        res.json({ noUser: "User not found" });
-        return;
-      }
+      const userLogin = client.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
 
-      const user = result.rows[0];
-      // const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-      const isPasswordCorrect = password === user.password;
+        if (result.rows.length === 0) {
+          res.json({ noUser: "User not found" });
+          return;
+        }
 
-      if (!isPasswordCorrect) {
-        res.json({ wrongPassword: "Incorrect password" });
-        return;
-      }
+        const user = result.rows[0];
+        // const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+        const isPasswordCorrect = password === user.password;
 
-      const token = jwt.sign(
-        { email: user.email, role: user.role },
-        process.env.JWT_SECRET,
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({ token: token, user: user });
-    });
+        if (!isPasswordCorrect) {
+          res.json({ wrongPassword: "Incorrect password" });
+          return;
+        }
+
+        const token = jwt.sign(
+          { email: user.email, role: user.role },
+          process.env.JWT_SECRET,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ token: token, user: user });
+      });
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }

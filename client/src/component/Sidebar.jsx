53fc9eec9ca5b46709333b566fda64bd662d4fac
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-21 22:00:10 +0545

    sidebar links added

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess } from "../state";
 const Sidebar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [role, setRole] = useState("");
   const [styleList, setStyleList] = useState(
     `mt-2 py-2 px-6 bg-gray-900 bg-opacity-30 text-lg text-gray-400  border-gray-900 rounded-full hover:bg-opacity-50 hover:text-gray-100 cursor-pointer  `
   );
+  const [activeStyle, setActiveStyle] = useState(
+    `mt-2 py-2 px-6 bg-gray-900 bg-opacity-50 text-lg text-gray-100  border-gray-900 rounded-full  cursor-pointer  `
+  );
+
+  const classid = localStorage.getItem("user");
+  const cid = JSON.parse(classid).cid;
 
   const logout = () => {
     console.log("logout");
     localStorage.removeItem("token");
     localStorage.removeItem("user");
+    dispatch(loginSuccess(""));
     navigate("/");
   };
 
@@ -38,19 +47,30 @@ const Sidebar = () => {
       {role === "student" ? (
         <div>
           <ul>
-            <li className={styleList}>
-              <span className="">Subjects</span>
-            </li>
-            <li className={styleList}>
-              <span className="">Assignments</span>
-            </li>
-            <li className={styleList}>
-              <span className="">Notes</span>
-              {/* student view notes */}
-            </li>
-            <li className={styleList}>
-              <span className="">Notice</span>
-            </li>
+            <Link to={`/class/subjects/${cid}`}>
+              <li className={styleList}>
+                <span className="">Subjects</span>
+              </li>
+            </Link>
+
+            <Link to={`/class/subjects/${cid}`}>
+              <li className={styleList}>
+                <span className="">Assignments</span>
+              </li>
+            </Link>
+
+            <Link to={`/class/subjects/${cid}`}>
+              <li className={styleList}>
+                <span className="">Notes</span>
+                {/* student view notes */}
+              </li>
+            </Link>
+
+            <Link to={`/notice`}>
+              <li className={styleList}>
+                <span className="">Notice</span>
+              </li>
+            </Link>
 
             <li className={styleList} onClick={logout}>
               <span className="">Logout</span>
@@ -74,10 +94,11 @@ const Sidebar = () => {
               <span className="">Notes</span>
               {/* teacher upload note and view */}
             </li>
-            <li className={styleList}>
-              <span className="">Notice</span>
-              {/* view notice */}
-            </li>
+            <Link to={`/notice`}>
+              <li className={styleList}>
+                <span className="">Notice</span>
+              </li>
+            </Link>
 
             <li className={styleList} onClick={logout}>
               <span className="">Logout</span>
@@ -91,21 +112,23 @@ const Sidebar = () => {
       {role === "admin" ? (
         <div>
           <ul>
-            <li className={styleList}>
-              <span className="">Class</span>
-              {/* add and view students in particular class*/}
-            </li>
+            <Link to={`/dashboard`}>
+              <li className={styleList}>
+                <span className="">Class</span>
+                {/* add and view students in particular class*/}
+              </li>
+            </Link>
+
+            {/* <Link to={`/teachers`}></Link> */}
             <li className={styleList}>
               <span className="">Teachers</span>
             </li>
-            <li className={styleList}>
-              <span className="">Add Class </span>
-              {/* teacher upload note and view */}
-            </li>
-            <li className={styleList}>
-              <span className="">Notice</span>
-              {/* add notice */}
-            </li>
+
+            <Link to={`/notice`}>
+              <li className={styleList}>
+                <span className="">Notice</span>
+              </li>
+            </Link>
 
             <li className={styleList} onClick={logout}>
               <span className="">Logout</span>

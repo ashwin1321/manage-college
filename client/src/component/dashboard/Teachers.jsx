a9d2a312dfa1cc8e345fd2480bebb1b4cee77d62
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-23 16:18:17 +0545

    list teachers pagination done

--- client/src/component/dashboard/Teachers.jsx
+++ client/src/component/dashboard/Teachers.jsx
@@ -70,6 +70,111 @@ const Teachers = () => {
       });
   };
 
+  // paginate
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+  const totalPages = Math.ceil(teachers.length / itemsPerPage);
+
+  const handlePageClick = (pageNumber) => {
+    if (pageNumber < 1) pageNumber = 1;
+    if (pageNumber > totalPages) pageNumber = totalPages;
+
+    setCurrentPage(pageNumber);
+  };
+
+  const renderTableData = () => {
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const slicedData = teachers.slice(start, end);
+
+    return slicedData.map((teacher, index) => {
+      return (
+        <tr key={index + 1}>
+          <td className="p-3 text-gray-600 text-center border border-b block lg:table-cell relative lg:static">
+            {teacher.tid}
+          </td>
+          <td className="p-3 text-gray-600 text-center border border-b block lg:table-cell relative lg:static">
+            {teacher.name}
+          </td>
+          <td className="p-3 text-gray-600 text-center border border-b block lg:table-cell relative lg:static">
+            {teacher.email}
+          </td>
+          <td className="p-3 text-gray-600 justify-center items-center border border-b block lg:table-cell relative lg:static">
+            {rolee === "admin" ? (
+              <div className="flex fkex-row justify-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5  text-red-700 hover:text-red-400 cursor-pointer"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5 text-green-700 hover:text-green-400 cursor-pointer"
+                  onClick={() => {
+                    setIsAssign(true);
+                    setTeacherId(teacher.tid);
+                  }}
+                >
+                  <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                </svg>
+              </div>
+            ) : null}
+          </td>
+        </tr>
+      );
+    });
+  };
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li
+          key={i}
+          onClick={() => handlePageClick(i)}
+          className={`inline-block mx-1 cursor-pointer rounded ${
+            currentPage === i
+              ? "bg-blue-500 text-white"
+              : "bg-white text-blue-500 hover:bg-gray-200"
+          }`}
+        >
+          {i}
+        </li>
+      );
+    }
+    return (
+      <div className="flex justify-center mt-4 gap-2">
+        <button
+          className={`bg-white hover:bg-blue-600 text-blue-500 hover:text-white font-semibold px-2  border border-gray-400 rounded shadow ${
+            currentPage === 1 && "opacity-50 cursor-not-allowed"
+          }`}
+          onClick={() => handlePageClick(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          className={`bg-white hover:bg-blue-600 text-blue-500 hover:text-white font-semibold  px-2 border border-gray-400 rounded shadow ${
+            currentPage === totalPages && "opacity-50 cursor-not-allowed"
+          }`}
+          onClick={() => handlePageClick(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
+    );
+  };
+
   useEffect(() => {
     const getTeachers = async () => {
       const res = await axios
@@ -248,52 +353,9 @@ const Teachers = () => {
                 </th>
               </tr>
             </thead>
-            <tbody>
-              {teachers.map((teacher) => (
-                <tr key={teacher.tid}>
-                  <td className="p-3 text-gray-600 text-center border border-b block lg:table-cell relative lg:static">
-                    {teacher.tid}
-                  </td>
-                  <td className="p-3 text-gray-600 text-center border border-b block lg:table-cell relative lg:static">
-                    {teacher.name}
-                  </td>
-                  <td className="p-3 text-gray-600 text-center border border-b block lg:table-cell relative lg:static">
-                    {teacher.email}
-                  </td>
-                  <td className="p-3 text-gray-600 justify-center items-center border border-b block lg:table-cell relative lg:static">
-                    {rolee === "admin" ? (
-                      <div className="flex fkex-row justify-center gap-2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          className="w-5 h-5  text-red-700 hover:text-red-400 cursor-pointer"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          className="w-5 h-5 text-green-700 hover:text-green-400 cursor-pointer"
-                          onClick={() => {
-                            setIsAssign(true);
-                            setTeacherId(teacher.tid);
-                          }}
-                        >
-                          <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                        </svg>
-                      </div>
-                    ) : null}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+            <tbody>{renderTableData()}</tbody>
           </table>
+          {renderPageNumbers()}
         </div>
       )}
 

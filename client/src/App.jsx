50004dad6cdb6a850f5015b95b18360131b9b429
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-23 13:02:32 +0545

    users access according to roles routes set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import Login from "./pages/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  redirect,
+} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import StudentLogin from "./pages/LoggedIn/StudentLogin";
 import ViewClass from "./pages/LoggedIn/ViewClass";
@@ -19,16 +24,60 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Homepage />} />
-        <Route path="/classView" element={<StudentLogin />} />
-        <Route path="/class/:id/view" element={<ViewClass />} />
-        <Route path="/class/subjects/:idd" element={<ListSubjects />} />
-        <Route path="/class/students/:idd" element={<ListStudents />} />
+
+        <Route
+          path="/classView"
+          element={
+            <IsAdmin>
+              <StudentLogin />
+            </IsAdmin>
+          }
+        />
+        <Route
+          path="/class/:id/view"
+          element={
+            <IsAdmin>
+              <ViewClass />
+            </IsAdmin>
+          }
+        />
+        <Route
+          path="/class/subjects/:idd"
+          element={
+            <IsAdminOrStudent>
+              <ListSubjects />
+            </IsAdminOrStudent>
+          }
+        />
+
+        <Route
+          path="/class/students/:idd"
+          element={
+            <IsAdmin>
+              <ListStudents />
+            </IsAdmin>
+          }
+        />
         <Route path="/class/:cid/subjects/view" element={<InsideSubjects />} />
         <Route path="/subject/assignment/:idd" element={<ListAssignments />} />
         <Route path="/subject/notes/:idd" element={<ListNotes />} />
         <Route path="/notice" element={<ListNotices />} />
-        <Route path="/teachers" element={<ListTeachers />} />
-        <Route path="/teachers/subjects/:tidd" element={<TeacherSubjects />} />
+        <Route
+          path="/teachers"
+          element={
+            <IsAdmin>
+              <ListTeachers />
+            </IsAdmin>
+          }
+        />
+        <Route
+          path="/teachers/subjects/:tidd"
+          element={
+            <IsTeacherOrAdmin>
+              <TeacherSubjects />
+            </IsTeacherOrAdmin>
+          }
+        />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
@@ -36,3 +85,47 @@ function App() {
 }
 
 export default App;
+
+export function IsAdmin(props) {
+  const user = localStorage.getItem("user");
+  const role = JSON.parse(user).role;
+  if (role === "admin") {
+    return props.children;
+  } else {
+    return (
+      <div className=" text-center text-3xl mt-[20%] text-red-800">
+        <h1>Not Authorized! </h1>
+        <h1>Please go back!!</h1>
+      </div>
+    );
+  }
+}
+export function IsAdminOrStudent(props) {
+  const user = localStorage.getItem("user");
+  const role = JSON.parse(user).role;
+  if (role === "admin" || role === "student") {
+    return props.children;
+  } else {
+    return (
+      <div className=" text-center text-3xl mt-[20%] text-red-800">
+        <h1>Not Authorized! </h1>
+        <h1>Please go back!!</h1>
+      </div>
+    );
+  }
+}
+
+export function IsTeacherOrAdmin(props) {
+  const user = localStorage.getItem("user");
+  const role = JSON.parse(user).role;
+  if (role === "admin" || role === "teacher") {
+    return props.children;
+  } else {
+    return (
+      <div className=" text-center text-3xl mt-[20%] text-red-800">
+        <h1>Not Authorized! </h1>
+        <h1>Please go back!!</h1>
+      </div>
+    );
+  }
+}

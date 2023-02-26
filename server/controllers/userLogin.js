f5634e54abca12ac4a4dcddf919e9fa2bdc1417e
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-26 22:48:39 +0545

    parameterized query

--- server/controllers/userLogin.js
+++ server/controllers/userLogin.js
@@ -5,191 +5,148 @@ const jwt = require("jsonwebtoken");
 connectDb();
 
 exports.loginUser = async (req, res) => {
-  try {
-    const { email, password, role } = req.body;
+    try {
+        const { email, password, role } = req.body;
 
-    if (role === "admin") {
-      const query = `select * from users where email = '${email}'`;
+        if (role === "admin") {
+            const query = `select * from users where email = $1`;
+            const values = [email];
 
-      const userLogin = client.query(query, (err, result) => {
-        if (err) {
-          console.log(err);
-          return;
+            const result = await client.query(query, values)
+
+            if (result.rows.length === 0) {
+                res.status(404).json({ noUser: "User not found" });
+                return;
+            }
+
+            const user = result.rows[0];
+            const isPasswordCorrect = password === user.password;
+
+            if (!isPasswordCorrect) {
+                res.status(404).json({ wrongPassword: "Incorrect password" });
+                return;
+            }
+
+            const token = jwt.sign(
+                { email: user.email, role: user.role },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.status(201).json({ token: token, user: user });
+
         }
 
-        if (result.rows.length === 0) {
-          res.json({ noUser: "User not found" });
-          return;
+        if (role === "teacher") {
+            const query = `select * from teachers where email = $1`;
+            const values = [email];
+
+            const result = await client.query(query, values)
+
+            if (result.rows.length === 0) {
+                res.status(404).json({ noUser: "User not found" });
+                return;
+            }
+
+            const user = result.rows[0];
+            const isPasswordCorrect = password === user.password;
+
+            if (!isPasswordCorrect) {
+                res.status(404).json({ wrongPassword: "Incorrect password" });
+                return;
+            }
+
+            const token = jwt.sign(
+                { email: user.email, role: user.role },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.status(201).json({ token: token, user: user });
         }
 
-        const user = result.rows[0];
-        // const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-        const isPasswordCorrect = password === user.password;
+        if (role === "student") {
+            const query = `select * from students where email = $1`;
+            const values = [email];
 
-        if (!isPasswordCorrect) {
-          res.json({ wrongPassword: "Incorrect password" });
-          return;
+            const result = await client.query(query, values)
+
+            if (result.rows.length === 0) {
+                res.status(404).json({ noUser: "User not found" });
+                return;
+            }
+
+            const user = result.rows[0];
+            const isPasswordCorrect = password === user.password;
+
+            if (!isPasswordCorrect) {
+                res.status(404).json({ wrongPassword: "Incorrect password" });
+                return;
+            }
+
+            const token = jwt.sign(
+                { email: user.email, role: user.role },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.status(201).json({ token: token, user: user });
+
         }
-
-        const token = jwt.sign(
-          { email: user.email, role: user.role },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({ token: token, user: user });
-      });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-
-    if (role === "teacher") {
-      const query = `select * from teachers where email = '${email}'`;
-
-      const teacherLogin = client.query(query, (err, result) => {
-        if (result.rows.length === 0) {
-          res.json({ noUser: "User not found" });
-          return;
-        }
-
-        const user = result.rows[0];
-        // const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-        const isPasswordCorrect = password === user.password;
-
-        if (!isPasswordCorrect) {
-          res.json({ wrongPassword: "Incorrect password" });
-          return;
-        }
-
-        const token = jwt.sign(
-          { email: user.email, role: user.role },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({ token: token, user: user });
-      });
-    }
-
-    if (role === "student") {
-      const query = `select * from students where email = '${email}'`;
-
-      const studentLogin = client.query(query, (err, result) => {
-        if (result.rows.length === 0) {
-          res.json({ noUser: "User not found" });
-          return;
-        }
-
-        const user = result.rows[0];
-        // const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-        const isPasswordCorrect = password === user.password;
-
-        if (!isPasswordCorrect) {
-          res.json({ wrongPassword: "Incorrect password" });
-          return;
-        }
-
-        const token = jwt.sign(
-          { email: user.email, role: user.role },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({ token: token, user: user });
-      });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
 };
 
 exports.registerUser = async (req, res) => {
-  try {
-    const { role } = req.body;
+    try {
+        const { role } = req.body;
 
-    if (role === "teacher") {
-      const { tid, name, email, password } = req.body;
-      let query = `insert into teachers(tid,  name, email, password, role) values('${tid}',  '${name}', '${email}', '${password}', '${role}')`;
+        if (role === "teacher") {
+            const { tid, name, email, password } = req.body;
+            let query = `insert into teachers(tid,  name, email, password, role) values($1, $2, $3, $4, $5)`;
+            const values = [tid, name, email, password, role]
 
-      const addTeacher = client.query(query, (err, result) => {
-        if (err) {
-          console.log(err);
-          return;
+            const result = await client.query(query, values)
+            res.status(201).json("teacher added successfully");
+
         }
-        res.status(200).json("teacher added successfully");
-      });
-    }
-    if (role === "student") {
-      const { stuid, cid, name, email, password } = req.body;
-      let query = `insert into students(stuid, cid, name, email, password, role) values('${stuid}', '${cid}', '${name}', '${email}', '${password}', '${role}')`;
 
-      const addTeacher = client.query(query, (err, result) => {
-        if (err) {
-          console.log(err);
-          return;
+        if (role === "student") {
+            const { stuid, cid, name, email, password } = req.body;
+            let query = `insert into students(stuid, cid, name, email, password, role) values($1, $2, $3, $4, $5, $6)`;
+            const values = [stuid, cid, name, email, password, role]
+
+            const result = await client.query(query, values)
+            res.status(201).json("student added successfully");
         }
-        res.status(200).json("student added successfully");
-      });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
 };
 
-exports.changePassword = async (req, res) => {
-  // change pasword hash and store
-  // =============== REMAINING ===============
-  // try {
-  //   const { email, password } = req.body;
-  //   const query = `select * from users where email = '${email}'`;
-  //   const userExists = client.query(query, (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return;
-  //     }
-  //     if (result.rows.length === 0) {
-  //       res.status(200).json({ noUser: "User not found" });
-  //     }
-  //     const user = result.rows[0];
-  //     const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-  //     if (!isPasswordCorrect) {
-  //       res.status(200).json({ wrongPassword: "Incorrect password" });
-  //     }
-  //   });
-  // } catch (err) {
-  //   res.status(404).json({ error: err.message });
-  // }
-  res.send("change password");
-};
 
 exports.getStudents = async (req, res) => {
-  const { id } = req.params;
-  console.log(id);
+    const { id } = req.params;
+    console.log(id);
 
-  try {
-    const query = `select stuid,cid,name,email from students where cid = '${id}'`;
+    try {
+        const query = `select stuid,cid,name,email from students where cid = $1`;
+        const values = [id];
 
-    const viewStudent = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+        const result = await client.query(query, values)
+        res.status(200).json(result.rows);
 
-      res.json(result.rows);
-    });
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 exports.getTeachers = async (req, res) => {
-  try {
-    const query = `select tid,name,email from teachers`;
+    try {
+        const query = `select tid,name,email from teachers`;
 
-    const getTeach = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+        const result = await client.query(query)
+        res.status(200).json(result.rows);
 
-      res.json(result.rows);
-    });
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
--- server/routes/userLogin.js
+++ server/routes/userLogin.js
@@ -13,6 +13,6 @@ router.get("/view-students/:id", getStudents); // get user details
 router.get("/view-teachers", getTeachers); // get user details
 router.post("/login", loginUser);
 router.post("/register", registerUser); // only admin can add or delete     // register to respective table
-router.patch("/change-password", changePassword);
+// router.patch("/change-password", changePassword);
 
 module.exports = router;

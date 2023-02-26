e27fe69d9880021e0a9a59dcd74fc64bb1783b77
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-26 22:04:10 +0545

    parameterized query and callback removed

--- server/controllers/assignments.js
+++ server/controllers/assignments.js
@@ -2,56 +2,47 @@ const { connectDb, client } = require("../models/config");
 
 //  view assignments
 exports.getAssignment = async (req, res) => {
-  try {
-    const { sid } = req.params;
-    const query = `select * from assignments where sid = '${sid}'`;
+    try {
+        const { sid } = req.params;
+        const query = `select * from assignments where sid = $1`;
+        const values = [sid];
 
-    const viewAssignments = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.status(200).json(result.rows);
-    });
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+        const result = await client.query(query, values);
+        res.status(200).json(result.rows);
+
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
 };
 
 //  add assignments
 exports.addAssignment = async (req, res) => {
-  try {
-    const { assignment, note, sid } = req.body;
+    try {
+        const { assignment, note, sid } = req.body;
 
-    const query = `insert into assignments (sid, assignment, note) values ('${sid}', '${assignment}', '${note}')`;
+        const query = `insert into assignments (sid, assignment, note) values ($1, $2, $3)`;
+        const values = [sid, assignment, note];
 
-    const addintodb = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.status(200).json({ message: "Assignment added" });
-    });
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+        const result = await client.query(query, values);
+        res.status(201).json({ message: "Assignment added" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 // delete subject
 exports.deleteAssignment = async (req, res) => {
-  try {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const query = `delete from assignments where id = '${id}'`;
+        const query = `delete from assignments where id = $1`;
+        const values = [id];
 
-    const delassignment = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.status(200).json({ message: "Assignment deleted" });
-    });
-  } catch (err) {
-    res.status(404).json({ error: err.message });
-  }
+        const result = await client.query(query, values);
+        res.status(200).json({ message: "Assignment deleted" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };

b7ec1843b345ee9d455b4ec139e33a6f9c265ac1
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-26 22:15:19 +0545

    parameterized query added and callbacks handled

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -2,58 +2,51 @@ const { connectDb, client } = require("../models/config");
 
 //  view notes
 exports.getNotes = async (req, res) => {
-  try {
-    const { sid } = req.params;
-    const query = `SELECT * FROM notes where sid = '${sid}'`;
+    try {
+        const { sid } = req.params;
+        const query = `SELECT * FROM notes where sid = $1`;
+        const values = [sid];
 
-    const viewNote = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
+        const result = await client.query(query, values)
+
         res.status(200).json(result.rows);
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
 
 //  add notes=
 exports.addNotes = async (req, res) => {
-  try {
-    const { sid } = req.params;
-    const { note } = req.body;
-    console.log(sid, note);
+    try {
+        const { sid } = req.params;
+        const { note } = req.body;
 
-    const query = `INSERT INTO notes (sid, note) VALUES ('${sid}', '${note}')`;
+        const query = `INSERT INTO notes (sid, note) VALUES ($1, $2)`;
+        const values = [sid, note];
 
-    const addintodb = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.status(200).json({ message: "Note added" });
-      }
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+        const result = client.query(query, values)
+
+        res.status(201).json({ message: "Note added" });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 // delete notes
 exports.deleteNotes = async (req, res) => {
-  try {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const query = `DELETE FROM notes WHERE id = '${id}'`;
+        const query = `DELETE FROM notes WHERE id = $1`;
+        const values = [id];
 
-    const delnote = client.query(query, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
+        const result = await client.query(query, values)
+
         res.status(200).json({ message: "Note deleted" });
-      }
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
